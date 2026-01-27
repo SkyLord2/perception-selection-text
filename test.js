1f533ccbfda66b75f1e1ed7cdb33a172fc32ab8d
SkyLord2 <chengdongsheng@outlook.com>
2026-01-27 21:26:32 +0800

    测试脚本

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-const { plus100 } = require('./index.js')
+const { doInitialize } = require('./index.js')
 
-console.log(plus100(5))
+doInitialize(
+  (err, info) => {
+    console.log('文字选中上报:', info)
+  },
+  (err, log) => {
+    console.log(log)
+  },
+)
+
+setInterval(
+  () => {
+    console.log('一小时过去了')
+  },
+  1000 * 60 * 60,
+)
